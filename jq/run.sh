e5e8e404c8e25e3b127d5e6c96e5e294dbc1e3f6
Chris <schauer@inf-schauer.de>
2024-06-29 10:49:25 +0200

    jq: docu and variable search term

--- jq/run.sh
+++ jq/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-jq -r 'to_entries[] | select(.value.path == "/mnt/path") | .key' input.json
+# search for path
+search_val_path="/mnt/path"
+
+# to_entries[]: Converts the JSON object into an array of key-value pairs.
+# select(.value.path == "/mnt/path"): Filters the array to select the object where the path is equal to "/mnt/path".
+# .key: Retrieves the key of the selected object.
+# -r: Outputs the result as raw text, without quotes.
+
+jq -r --arg path "$search_val_path" 'to_entries[] | select(.value.path == $path) | .key' input.json
