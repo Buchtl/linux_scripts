8673cb30f12343b3790f37e9872d5712b629a182
Chris <schauer@inf-schauer.de>
2025-08-10 13:36:39 +0200

    idea

---
+++ intellij/install.desktop.sh
@@ -0,0 +1,17 @@
+
+
+cat <<EOF > ~/.local/share/applications/intellij-idea-ultimate.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=IntelliJ IDEA Ultimate
+Icon=/opt/software/idea-IU/bin/idea.png
+Exec="/opt/software/idea-IU/bin/idea.sh" %f
+Comment=The Ultimate Edition of IntelliJ IDEA
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-idea
+EOF
+
+chmod +x ~/.local/share/applications/intellij-idea-ultimate.desktop
+update-desktop-database ~/.local/share/applications
